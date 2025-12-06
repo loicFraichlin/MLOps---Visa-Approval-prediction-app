5ac22081694989fe631cebb25cde84549b7c869b
loicFraichlin <ngongangloic151@gmail.com>
2025-12-06 16:17:12 +0100

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="visa_approval_prediction_app",
+    version="0.0.0",
+    author="loicFraichlin",
+    author_email="ngongangloic151@gmail.com",
+    packages=find_packages()
+)
